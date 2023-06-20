2eead5e35cb885da373de24f1a5ce3b93f9193eb
horombacollin <collinhorombaorg>
2023-06-20 13:10:54 +0200

    nice commit

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+let d = new Date();
+
+let newUpdate = document.getElementById('footer-sect');
+newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
+newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
